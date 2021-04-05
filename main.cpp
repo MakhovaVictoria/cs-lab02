c650332842457e616a4cc52c6cf2b69ea5095aed
Alice (MakhovaVA) <makhova@mail.com>
2021-04-05 10:39:21 +0300

    code: добавлен вывод произведения A и B

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main ()
 cout << "Enter A and B: ";
 int a, b;
 cin >> a >> b;
-cout << "A + B = " << a + b << '\вывод суммы'
+cout << "A + B = " << a + b << '\vivid summi'
+     << "A - B = " << a - b << '\vivod proizvedenia';
     return 0;
 }
